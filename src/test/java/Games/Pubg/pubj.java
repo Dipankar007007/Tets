0c1576b3c4746b05d659f5b34a49dbac3b62d2bb
Dipankar007007 <psinghcareer93@gmail.com>
2022-01-11 18:42:08 +0530

    From india

--- src/test/java/Games/Pubg/pubj.java
+++ src/test/java/Games/Pubg/pubj.java
@@ -14,4 +14,13 @@ public class pubj {
 		
 		System.out.println("I am from America ");
 	}
+	
+	public void m2devlop()
+	{
+		System.out.println("m2 is devloping from developer branch ");
+	}
+	public void m3devlop()
+	{
+		System.out.println("m3 is devloping from developer branch ");
+	}
 }
